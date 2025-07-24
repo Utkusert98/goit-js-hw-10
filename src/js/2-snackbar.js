d942edd420dcf8ab3565ebd48ac5071f3b081c06
Utku Sert <utkusert.arc@gmail.com>
2025-07-24 10:46:11 +0300

    neww-1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,41 +1,30 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
+if (form) {
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const delay = Number(form.elements.delay.value);
+    const state = form.elements.state.value;
 
-  const delay = Number(form.elements.delay.value);
-  const state = form.elements.state.value;
-
-  createPromise(delay, state)
-    .then(delayValue => {
-      iziToast.success({
-        title: 'Success',
-        message: `✅ Fulfilled promise in ${delayValue}ms`,
-        position: 'topRight',
-      });
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        state === 'fulfilled' ? resolve(delay) : reject(delay);
+      }, delay);
     })
-    .catch(delayValue => {
-      iziToast.error({
-        title: 'Error',
-        message: `❌ Rejected promise in ${delayValue}ms`,
-        position: 'topRight',
+      .then(ms => {
+        iziToast.success({
+          message: `✅ Fulfilled promise in ${ms}ms`,
+          position: "topRight",
+        });
+      })
+      .catch(ms => {
+        iziToast.error({
+          message: `❌ Rejected promise in ${ms}ms`,
+          position: "topRight",
+        });
       });
-    });
-
-  form.reset();
-});
-
-function createPromise(delay, state) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (state === 'fulfilled') {
-        resolve(delay);
-      } else {
-        reject(delay);
-      }
-    }, delay);
   });
 }
