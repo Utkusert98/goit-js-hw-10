6ecc257f6c01c0d446ed27b3fca7bc6c90dd91d4
Utku Sert <utkusert.arc@gmail.com>
2025-07-30 12:52:32 +0300

    fix: root yapı ve vite config güncellemesi

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
+    root: '.', // Proje kökü ana klasör (config ve index.html burada)
     build: {
-      sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: './index.html', // Ana giriş noktası root'ta index.html
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -34,12 +29,16 @@ export default defineConfig(({ command }) => {
           },
         },
       },
-      outDir: '../dist',
+      outDir: 'dist', // Build sonrası dosyalar dist klasöründe olacak
       emptyOutDir: true,
+      sourcemap: true,
+    },
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/**.html']),
+      FullReload(['./**/*.html']),
       SortCss({
         sort: 'mobile-first',
       }),
